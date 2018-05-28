81016718c2269ea7a6a9e073f94fb16c7e98a589
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2018-05-28 17:00:35 +0300

    add models, controllers, relationship

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Task;
+use App\Http\Requests\TaskRequest;
+
+class TaskController extends Controller
+{
+	public function __construct()
+	{
+		$this->middleware('auth');
+	}
+
+	/**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request, Task $task)
+    {
+        //get all the tasks based on current user id
+		$allTasks = $task->whereIn('user_id', $request->user())->width('user');
+    	$tasks = $allTasks->orderBy('created_at','desc')->take(20)->get();
+
+    	return response()->json([
+			'tasks' => $tasks,
+		]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(TaskRequest $request)
+    {
+        $task = $request->user()->tasks()->create([
+        	'name' => $request->name
+		]);
+
+        return response()->json($task->width('user')->find($task->id));
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
