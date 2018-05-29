0ad39a5e4acb256a170a7be26ab93ad5935af68f
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2018-05-29 17:43:52 +0300

    add router

--- readme.md
+++ readme.md
@@ -3,3 +3,4 @@ Laravel project, use React
 - php artisan preset react
 - npm install
 - php artisan migrate
+- npm install react-router-dom
--- resources/assets/js/index.js
+++ resources/assets/js/index.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import {BrowserRouter, Switch, Route, Link} from 'react-router-dom';
 
 
 if (document.getElementById('root')) {
-    ReactDOM.render(<App />, document.getElementById('root'));
+    ReactDOM.render(
+        <BrowserRouter>
+            <div>
+                <Switch>
+                    <App />
+                    <Route exact path="/" component={App} />
+                </Switch>
+            </div>
+        </BrowserRouter>
+            , document.getElementById('root'));
 }
